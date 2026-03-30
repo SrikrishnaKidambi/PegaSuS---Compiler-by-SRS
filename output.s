5013dc14dd1adb39b4031cbb198c1eae22b57884
Raghavendra <raghava7779@gmail.com>
2026-03-30 05:45:26 +0000

    reimplemeted lost changes

--- output.s
+++ output.s
@@ -2,56 +2,25 @@
 
     .section .text
     .globl main
-
-    add:
-    # -- prologue --
-        addi   sp, sp, -80
-        sw     ra, 76(sp)
-        sw     s0, 72(sp)
-        addi   s0, sp, 80
-    # ISSUE: symbol not found for addres lookup
-        sw     a0, 0(s0)
-    # ISSUE: symbol not found for addres lookup
-        sw     a1, 0(s0)
-    # -- prologue end --
-
-    # x
-    # y
+    # assign
+        li   t1, 20
     # +
-    # ISSUE: symbol not found for addres lookup
-    lw t1, 0(s0) # unknown: x
-    # ISSUE: symbol not found for addres lookup
-    lw t2, 0(s0) # unknown: y
+    li t2, 20
         add  t3, t1, t2
-    # ISSUE: symbol not found for addres lookup
-    sw t3, 0(s0) # unknown: t0
-    # ISSUE: symbol not found for addres lookup
-        lw     a0, 0(s0)
-        j      .Lepi_add
-    .Lepi_add:
-    # -- epilogue --
-        lw     ra, 76(sp)
-        lw     s0, 72(sp)
-        addi   sp, sp, 80
-        ret
-    # -- epilogue end --
-
     # assign
-        li   t1, 0
-    sw t1, 0(s0)
-    # spill all registers
-        li     a0, 5
-        li     a1, 6
-        call   add
-    # ISSUE: symbol not found for addres lookup
-        sw     a0, 0(s0)
+        mv   t4, t3
+    # *
+        mul  t5, t4, t1
     # assign
-    # ISSUE: symbol not found for addres lookup
-    lw t1, 0(s0) # unknown: t1
-        mv   t2, t1
-    sw t2, 0(s0)
+        mv   t6, t5
     # spill all registers
-        lw     a0, 0(s0)
+    # (debug) store ins. to spill
+    sw t1, -8(s0)
+    # (debug) store ins. to spill
+    sw t4, -12(s0)
+    # (debug) store ins. to spill
+    sw t6, -16(s0)
+        lw     a0, -16(s0)
         li     a7, 1
         ecall
 
