24a33521fff8438a2530a34b2e24d871170033b3
SrikrishnaKidambi <kidambivenkatasrikrishna@gmail.com>
2026-03-30 05:47:02 +0000

    minor changes

--- output.s
+++ output.s
@@ -2,56 +2,30 @@
 
     .section .text
     .globl main
-
-    add:
-    # -- prologue --
-        addi   sp, sp, -80
-        sw     ra, 76(sp)
-        sw     s0, 72(sp)
-        addi   s0, sp, 80
-    # ISSUE: symbol not found for addres lookup
-        sw     a0, 0(s0)
-    # ISSUE: symbol not found for addres lookup
-        sw     a1, 0(s0)
-    # -- prologue end --
-
-    # x
-    # y
-    # +
-    # ISSUE: symbol not found for addres lookup
-    lw t1, 0(s0) # unknown: x
-    # ISSUE: symbol not found for addres lookup
-    lw t2, 0(s0) # unknown: y
-        add  t3, t1, t2
-    # ISSUE: symbol not found for addres lookup
-    sw t3, 0(s0) # unknown: t0
-    # ISSUE: symbol not found for addres lookup
-        lw     a0, 0(s0)
-        j      .Lepi_add
-    .Lepi_add:
-    # -- epilogue --
-        lw     ra, 76(sp)
-        lw     s0, 72(sp)
-        addi   sp, sp, 80
-        ret
-    # -- epilogue end --
-
     # assign
-        li   t1, 0
+        li   t1, "raghavendra"
     sw t1, 0(s0)
-    # spill all registers
-        li     a0, 5
-        li     a1, 6
-        call   add
-    # ISSUE: symbol not found for addres lookup
-        sw     a0, 0(s0)
     # assign
+        li   t2, 20
+    sw t2, -8(s0)
+    # +
+    li t3, 20
+        add  t4, t2, t3
     # ISSUE: symbol not found for addres lookup
-    lw t1, 0(s0) # unknown: t1
-        mv   t2, t1
-    sw t2, 0(s0)
+    sw t4, 0(s0) # unknown: t0
+    # assign
+        mv   t2, t4
+    sw t2, -12(s0)
+    # *
+    lw t3, -8(s0)
+        mul  t4, t2, t3
+    # ISSUE: symbol not found for addres lookup
+    sw t4, 0(s0) # unknown: t1
+    # assign
+        mv   t2, t4
+    sw t2, -16(s0)
     # spill all registers
-        lw     a0, 0(s0)
+        lw     a0, -16(s0)
         li     a7, 1
         ecall
 
